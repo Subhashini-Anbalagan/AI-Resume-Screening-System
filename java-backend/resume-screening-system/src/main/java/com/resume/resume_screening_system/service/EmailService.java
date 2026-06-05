5d9d34221ad64f85bd2762303873d69c517b2003
Subhashini-Anbalagan <subhashinia28102002@gmail.com>
2026-06-05 18:28:57 +0530

    Fixed Gmail SMTP configuration

--- java-backend/resume-screening-system/src/main/java/com/resume/resume_screening_system/service/EmailService.java
+++ java-backend/resume-screening-system/src/main/java/com/resume/resume_screening_system/service/EmailService.java
@@ -100,6 +100,13 @@ public class EmailService {
 
     System.out.println("Trying SMTP Connection...");
 
+    System.out.println("FROM EMAIL = " + fromEmail);
+
+    System.out.println(
+            "APP PASSWORD LOADED = "
+                    + (System.getenv("GMAIL_APP_PASSWORD") != null)
+    );
+
     mailSender.send(message);
 
     System.out.println("SMTP Success");
